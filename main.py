d5c50152be66e9848bd6d3c2bd54273081347251
Charl P. Botha <cpbotha@vxlabs.com>
2025-06-20 09:49:50 +0200

    Add v1 of working script

---
+++ main.py
@@ -0,0 +1,97 @@
+# rss-to-signal send new (unseen) recent rss posts to signal using signal-cli
+# copyright 2025 Charl P. Botha <cpbotha@vxlabs.com>
+
+# process:
+# 1. download latest rss, using feedparser's etag mechanism
+# 2. process any items with date later than latest date, either latest from seen list or from argument
+# 3. for each item, prepare url, title, description, og:image for thumbnail so we can invoke signal-cli
+# notes:
+# we need to store seen post urls (the item ids) and their dates, as well as last etag
+# quickest to just maintain a json file which is deserialised into dict
+
+import datetime
+import json
+from pathlib import Path
+from typing import cast
+
+import feedparser
+from dateutil.parser import parse
+import typer
+
+LPED = "latest_processed_entry_date"
+
+
+def process_entry(e):
+    print(f"handling {e.guid} with {e.published}")
+
+
+def default(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+
+def _state_fn(profile: str):
+    return f"{profile}.state.json"
+
+
+def _config_fn(profile: str):
+    return f"{profile}.cfg.json"
+
+
+def dump_state(state, profile: str):
+    json.dump(state, Path(_state_fn(profile)).open("w"), indent=2, default=default)
+
+
+def object_hook(o):
+    if LPED in o:
+        o[LPED] = parse(o[LPED])
+    if "modified" in o:
+        o["modified"] = parse(o["modified"])
+    return o
+
+
+def main(profile: str):
+    cfg = json.load(Path(_config_fn(profile)).open())
+    feed_url = cfg["feed_url"]
+
+    try:
+        state = json.load(Path(_state_fn(profile)).open(), object_hook=object_hook)
+    except FileNotFoundError:
+        state = {}
+
+    # unpack state here
+    latest_processed_entry_date: datetime.datetime | None = state.get(LPED, None)
+    prev_feed_etag = state.get("etag")
+    prev_feed_modified = state.get("modified")
+
+    d = feedparser.parse(feed_url, etag=prev_feed_etag, modified=prev_feed_modified)
+    # https://feedparser.readthedocs.io/en/latest/http-etag.html
+    # get and store both, prefer to use etag
+    if d.status == 304:
+        print("Nothing has changed since the previous feed fetch")
+        return
+
+    for e in d.entries:
+        # published_parsed is a time.struct_time which has no TZ info
+        # so we rather parse our own
+        # print(e.id, parse(e.published).isoformat())
+        # print(json.dumps(e, indent=2))
+        e_date = parse(cast(str, e.published))
+        if latest_processed_entry_date is None or e_date > latest_processed_entry_date:
+            process_entry(e)
+            if state.get(LPED) is None or e_date > state[LPED]:
+                state[LPED] = e_date
+            dump_state(state, profile)
+
+    # we only save the etag / feed modified date if we've processed all the entries
+    if d.etag:
+        state["etag"] = d.etag
+    if d.modified:
+        state["modified"] = parse(cast(str, d.modified))
+
+    dump_state(state, profile)
+
+
+if __name__ == "__main__":
+    typer.run(main)
